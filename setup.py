7f995e6760dda39eb5ab5173649c5937c99e618a
Ubuntu <ubuntu@ip-10-48-158-194.eu-west-1.compute.internal>
2013-04-03 08:39:49 +0000

    Created ckan extension

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(
+	name='ckanext-openafrica',
+	version=version,
+	description="CKAN extension for Open Africa.",
+	long_description="""\
+	""",
+	classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+	keywords='',
+	author='David Lemayian',
+	author_email='david@openinstitute.com',
+	url='http://africaopendata.org',
+	license='MIT Open Licence.',
+	packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+	namespace_packages=['ckanext', 'ckanext.openafrica'],
+	include_package_data=True,
+	zip_safe=False,
+	install_requires=[
+		# -*- Extra requirements: -*-
+	],
+	entry_points=\
+	"""
+        [ckan.plugins]
+	# Add plugins here, eg
+	# myplugin=ckanext.openafrica:PluginClass
+	""",
+)
